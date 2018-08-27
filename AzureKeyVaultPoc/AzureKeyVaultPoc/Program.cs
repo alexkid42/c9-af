9b0405b1d494eb2a3bedaada9fadf081efc01fab
Alexandre Mellière <alexandre.melliere.prestataire@pro.cabestan.com>
2018-08-27 11:53:45 +0200

    Ajout de la solution.

---
+++ AzureKeyVaultPoc/AzureKeyVaultPoc/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace AzureKeyVaultPoc
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
